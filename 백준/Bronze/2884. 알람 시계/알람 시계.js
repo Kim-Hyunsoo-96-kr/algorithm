adfc7f4b578599884fbc4da1e7da0531fc11f93c
Kim Hyunsoo <103111913+Kim-Hyunsoo-96-kr@users.noreply.github.com>
2022-05-09 22:17:25 +0900

    [Bronze III] Title: 알람 시계, Time: 132 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+let hour = Number(input[0]);
+let minute = Number(input[1]);
+
+if(minute - 45 < 0) {
+    if(hour == 0) console.log(`23 ${minute+15}`);
+    else console.log(`${hour-1} ${minute+15}`);
+}
+else console.log(`${hour} ${minute-45}`);
