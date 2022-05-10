fead232ab20716eba5d9e1060fc3b78a317d2dfc
Kim Hyunsoo <103111913+Kim-Hyunsoo-96-kr@users.noreply.github.com>
2022-05-10 22:14:32 +0900

    [Bronze II] Title: 빠른 A+B, Time: 1304 ms, Memory: 164368 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let text = "";
+
+for(let i = 1; i <= Number(input[0]); i++) {
+    tmp = input[i].split(' ');
+    text += `${Number(tmp[0]) + Number(tmp[1])}\n`;
+}
+console.log(text);
