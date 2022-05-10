cab47c5d83da7f1acc3bb92b35ff3ada118026cc
Kim Hyunsoo <103111913+Kim-Hyunsoo-96-kr@users.noreply.github.com>
2022-05-10 22:05:29 +0900

    [Bronze III] Title: A+B - 3, Time: 124 ms, Memory: 9572 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+for(let i = 1; i <= Number(input[0]); i++) {
+    console.log(Number(input[i].split(' ')[0]) + Number(input[i].split(' ')[1]));
+}
