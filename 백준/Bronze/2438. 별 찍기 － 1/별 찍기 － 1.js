271f45ca0f9a5b307a852030020436c87dd923aa
Kim Hyunsoo <103111913+Kim-Hyunsoo-96-kr@users.noreply.github.com>
2022-05-10 22:28:46 +0900

    [Bronze III] Title: 별 찍기 - 1, Time: 120 ms, Memory: 9484 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+let num = Number(fs.readFileSync('/dev/stdin'));
+let text = "";
+
+for(let i = 1; i <= num; i++) {
+    for(let j = 1; j <= i; j++) {
+        text += '*';
+    }
+    text += '\n'
+}
+console.log(text);
