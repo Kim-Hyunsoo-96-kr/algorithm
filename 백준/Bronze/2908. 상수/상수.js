34c8cdeb890ea79eee8c13a15b216d449d637cf2
Kim Hyunsoo <103111913+Kim-Hyunsoo-96-kr@users.noreply.github.com>
2022-05-14 21:32:10 +0900

    [Bronze II] Title: 상수, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.js
+++ 백준/Bronze/2908. 상수/상수.js
@@ -4,8 +4,7 @@ let input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
 const num1 = reverseNum(input[0]);
 const num2 = reverseNum(input[1]);
 
-if (num1 > num2) console.log(num1);
-else console.log(num2);
+num1 > num2 ? console.log(num1) : console.log(num2);
 
 function reverseNum(numStr) {
   let tmpArr = numStr.split('');
