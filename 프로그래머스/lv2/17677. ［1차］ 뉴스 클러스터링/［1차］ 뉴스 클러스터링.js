0c0d0c152adcb18cfbc9edd1f3aece4dffd5fe23
Kim Hyunsoo <103111913+Kim-Hyunsoo-96-kr@users.noreply.github.com>
2022-07-27 21:53:13 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 5.04 ms, Memory: 30.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,52 +1,20 @@
 function solution(str1, str2) {
-  // const regex = /[^a-z]/g;
-  // console.log(str1.toLowerCase());
-  // console.log(str2.toLowerCase());
-  const set1 = depart(str1.toLowerCase());
-  const set2 = depart(str2.toLowerCase());
-
-  console.log(set1);
-  console.log(set2);
-  const intersection = {};
-  const union = {};
-
-  for (let x in set1) {
-    union[x] = set1[x];
-    while (set1[x] > 0) {
-      set1[x]--;
-      if (set2[x] !== undefined && set2[x] > 0) {
-        if (intersection[x] === undefined) intersection[x] = 1;
-        else intersection[x]++;
-        set2[x]--;
-      }
-    }
-  }
-  for (let x in set2) {
-    if (set2[x] > 0) {
-      if (union[x] === undefined) union[x] = set2[x];
-      else union[x] = union[x] + set2[x];
-    }
-  }
-  let intersectionCount = 0;
-  let unionCount = 0;
-  for (let x in intersection) {
-    intersectionCount += intersection[x];
-  }
-  for (let x in union) {
-    unionCount += union[x];
-  }
-    if(unionCount === 0) return 65536;
-  return Math.floor((intersectionCount / unionCount) * 65536);
+    let answer = 0;
+    let basis = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
+    
+    
+    return answer;
 }
 
-function depart(str1) {
-  let result = {};
-  const regex = /[^a-z]/g;
-
-  for (let i = 0; i < str1.length - 1; i++) {
-    if (str1[i].replace(regex, '') === '' || str1[i + 1].replace(regex, '') === '') continue;
-    if (result[str1[i] + str1[i + 1]] === undefined) result[str1[i] + str1[i + 1]] = 1;
-    else result[str1[i] + str1[i + 1]]++;
-  }
-  return result;
+function stringDecompose(str) {
+    let result = [];
+    
+    for(let i=0; i<str.length-1; i++) {
+        let tmpStr = '';
+        tmpStr += str[i];
+        tmpStr += str[i+1];
+        result.push(tmpStr);
+    }
 }
