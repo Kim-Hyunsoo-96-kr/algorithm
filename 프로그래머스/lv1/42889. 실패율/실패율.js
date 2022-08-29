8565bff783516c943621770e0a080c19cb78d02c
Kim Hyunsoo <103111913+Kim-Hyunsoo-96-kr@users.noreply.github.com>
2022-08-29 19:29:54 +0900

    [level 1] Title: 실패율, Time: 0.30 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,23 +1,31 @@
 function solution(N, stages) {
   let answer = [];
-  let players = {};
-  let playersNum = stages.length;
+  let stageObj = {};
+  let playerNum = stages.length;
 
   for (let i = 0; i < stages.length; i++) {
-    if (!players[stages[i]]) players[stages[i]] = 1;
-    else players[stages[i]]++;
+    if (!stageObj[stages[i]]) stageObj[stages[i]] = 1;
+    else stageObj[stages[i]]++;
   }
-  // console.log(players);
 
-  for (let i = 1; i <= N; i++) {
-    if (players[i]) {
-      let failRate = players[i] / playersNum;
-      playersNum -= players[i];
-      answer.push([i, failRate]);
-    } else answer.push([i, 0]);
+  let player = new Array(N + 2).fill(0);
+
+  for (let i = 1; i < player.length; i++) {
+    if (stageObj[i]) player[i] = stageObj[i];
   }
-  answer.sort((a, b) => b[1] - a[1]);
-  answer = answer.map((el) => el[0]);
-  // console.log(answer);
+
+  let failRate = new Array(N + 2).fill(0);
+  for (let i = 1; i < player.length; i++) {
+    failRate[i] = { index: i, failRate: player[i] / playerNum };
+    playerNum -= player[i];
+  }
+
+  failRate = failRate.slice(1, N + 1);
+
+  failRate = failRate.sort((a, b) => b.failRate - a.failRate);
+
+  failRate.forEach((el) => {
+    answer.push(el.index);
+  });
   return answer;
 }
