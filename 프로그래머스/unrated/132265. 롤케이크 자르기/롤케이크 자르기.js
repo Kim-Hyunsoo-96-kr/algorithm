975a6d1227d1b68ff6afb0a941e28cea42de3bb2
Kim Hyunsoo <103111913+Kim-Hyunsoo-96-kr@users.noreply.github.com>
2022-11-02 13:46:00 +0900

    [unrated] Title: 롤케이크 자르기, Time: 36.57 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -10,19 +10,10 @@ function solution(topping) {
 
   let leftToppingCount = 0;
   let rightToppingCount = Object.keys(rightTopping).length;
-  // console.log(toppingInfo);
 
-  // topping.forEach((el, idx) => {
-  //   if (!leftTopping[el]) leftTopping[el] = 1;
-  //   else leftTopping[el]++;
-
-  //   if (rightTopping[el] === 1) delete rightTopping[el];
-  //   else rightTopping[el]--;
-
-  //   if (Object.keys(leftTopping).length === Object.keys(rightTopping).length) answer++;
-  // });
-
-  topping.forEach((el) => {
+  
+  for (let i = 0; i < topping.length; i++) {
+    let el = topping[i];
     rightTopping[el]--;
     if (rightTopping[el] === 0) rightToppingCount--;
 
@@ -32,7 +23,7 @@ function solution(topping) {
     }
 
     if (leftToppingCount === rightToppingCount) answer++;
-    if (leftToppingCount > rightToppingCount) return false;
-  });
+    if (leftToppingCount > rightToppingCount) break;
+  }
   return answer;
 }
