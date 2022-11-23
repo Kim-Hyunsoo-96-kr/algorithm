a036345b47539c6ce0083909d1b522a3e8918b04
Kim Hyunsoo <103111913+Kim-Hyunsoo-96-kr@users.noreply.github.com>
2022-11-23 13:34:39 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,18 +1,37 @@
-function solution(n, words) {
-  let answer = [];
+// function solution(n, words) {
+//   let answer = [];
 
-  let store = {};
-  store[words[0]] = true;
+//   let store = {};
+//   store[words[0]] = true;
+//   for (let i = 1; i < words.length; i++) {
+//     if (store[words[i]] || words[i][0] !== words[i - 1][words[i - 1].length - 1]) {
+//       answer.push((i % n) + 1);
+//       answer.push(Math.floor(i / n) + 1);
+//         break;
+//     } else {
+//       store[words[i]] = true;
+//     }
+//   }
+//   if (answer.length === 0) answer = [0, 0];
+//   return answer;
+// }
+function solution(n, words) {
+  let answer = [0, 0];
+  words.unshift(null);
+  const wordStore = {};
+
   for (let i = 1; i < words.length; i++) {
-    if (store[words[i]] || words[i][0] !== words[i - 1][words[i - 1].length - 1]) {
-      answer.push((i % n) + 1);
-      answer.push(Math.floor(i / n) + 1);
+    if (i === 1) wordStore[words[i]] = true;
+    else {
+      if (wordStore[words[i]] || words[i - 1][words[i - 1].length - 1] !== words[i][0]) {
+        let number = i % n;
+        if (number === 0) number = n;
+        answer = [number, Math.ceil(i / n)];
         break;
-    } else {
-      store[words[i]] = true;
+      } else {
+        wordStore[words[i]] = true;
+      }
     }
   }
-  console.log(store);
-  if (answer.length === 0) answer = [0, 0];
   return answer;
 }
