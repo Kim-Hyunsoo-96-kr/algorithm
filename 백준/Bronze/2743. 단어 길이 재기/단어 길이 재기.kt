80588a864845434d9a2cba84ddf6190d87df6277
Kim Hyunsoo <103111913+Kim-Hyunsoo-96-kr@users.noreply.github.com>
2023-04-02 12:35:48 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 112 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.kt
@@ -0,0 +1,9 @@
+import java.io.*
+import kotlin.collections.*
+
+fun main() {
+    val bf = BufferedReader(InputStreamReader(System.`in`))
+    val input = bf.readText().trim()
+
+    println(input.length)
+}
