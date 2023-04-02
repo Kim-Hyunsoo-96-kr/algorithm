95c7caa7bf26e8b6a7aedd8b77be8bef26e38f83
Kim Hyunsoo <103111913+Kim-Hyunsoo-96-kr@users.noreply.github.com>
2023-04-02 12:50:35 +0900

    [Bronze V] Title: 아스키 코드, Time: 144 ms, Memory: 19972 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.kt
@@ -0,0 +1,8 @@
+import java.io.*
+import kotlin.collections.*
+
+fun main() {
+    val bf = BufferedReader(InputStreamReader(System.`in`))
+    val input = bf.readText().trim()
+    println(input.toByteArray().joinToString())
+}
