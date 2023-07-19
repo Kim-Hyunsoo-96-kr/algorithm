3cb17e27fdf09eb60e024284900c0bb5702d3b66
Kim Hyunsoo <103111913+Kim-Hyunsoo-96-kr@users.noreply.github.com>
2023-07-19 22:31:15 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1640 ms, Memory: 320336 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.kt
@@ -0,0 +1,18 @@
+import java.io.*
+
+fun main() {
+    val bf = BufferedReader(InputStreamReader(System.`in`))
+    var input = bf.readText().trim().split(System.lineSeparator()).drop(1).sortedWith(Comparator { o1, o2 ->
+        val tmp1 = o1.split(" ").map { it.toInt() }
+        val tmp2 = o2.split(" ").map { it.toInt() }
+
+        return@Comparator when {
+            tmp1[0] > tmp2[0] -> 1
+            tmp1[0] < tmp2[0] -> -1
+            tmp1[1] > tmp2[1] -> 1
+            tmp1[1] < tmp2[1] -> -1
+            else -> 0
+        }
+    })
+    println(input.joinToString(System.lineSeparator()))
+}
